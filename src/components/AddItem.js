609b2cc203bd6cf8600380f918fc4da17e221c52
daddyloyal <lifeofemmany6@gmail.com>
2023-01-03 10:10:48 +0100

    added search focus to the SearchItem component

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,12 +1,14 @@
 import { FaPlus } from 'react-icons/fa';
-import React from 'react'
+import { useRef } from 'react';
 
 const AddItem = ({ newItem, setNewItem, handleSubmit}) => {
+    const inputRef = useRef();
   return (
     <form className='addForm' onSubmit={handleSubmit}>
         <label htmlFor="addItem">Add Item</label>
         <input 
             autoFocus
+            ref={inputRef}
             id='addItem'
             type='text'
             placeholder='Add Item'
@@ -15,7 +17,8 @@ const AddItem = ({ newItem, setNewItem, handleSubmit}) => {
             onChange={(e) => setNewItem(e.target.value)} />
             <button 
                 type='submit'
-                aria-label='Add Item'>
+                aria-label='Add Item'
+                onClick={() => inputRef.current.focus()}>
                     <FaPlus />
             </button>
     </form>
